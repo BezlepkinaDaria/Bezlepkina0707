37a62002b3f43392638c7b8d783936b848d0852b
BezlepkinaDaria <daschab2015@gmail.com>
2024-11-08 18:20:20 +0300

    отчеты по практическим

--- PZ_3/PZ_3_2.py
+++ PZ_3/PZ_3_2.py
@@ -4,17 +4,20 @@
 #покупка < 2000 скидка 5%
 #составить программу, определяющую процентной ставки в зависимости от вносимой суммы
 
-purchase = float(input("Введите сумму покупки: "))
+try:
+    purchase = float(input("Введите сумму покупки: "))
 
-discount = 0 #скидка
+    discount = 0 #скидка
 
-if purchase < 500: #проверка условия
-    discount = purchase * 0.02 #скидка 2%
-elif purchase < 1000:
-    discount = purchase * 0.03 #скидка 3%
-elif purchase < 1500:
-    discount = purchase * 0.04 #скидка 4%
-else:
-    discount = purchase * 0.05 #скидка 5%
+    if purchase < 500:
+        discount = purchase * 0.02 #скидка 2%
+    elif purchase < 1000:
+        discount = purchase * 0.03 #скидка 3%
+    elif purchase < 1500:
+        discount = purchase * 0.04 #скидка 4%
+    elif purchase > 1500:
+        discount = purchase * 0.05 #скидка 5%
 
-print("Размер скидки составит:", discount)
+    print("Размер скидки составит:", discount)
+except ValueError:
+   print("Ошибка")
